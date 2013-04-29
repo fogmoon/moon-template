0cd01ca6b01b3a36fa61a32c1bba2ced14a0bc35
André Aubin <andre.aubin@lambdaweb.fr>
2013-04-29 23:25:22 +0200

    Adding a notification helper to the moonJs

--- js/moon.js
+++ js/moon.js
@@ -2,21 +2,59 @@
  * The moon javascript helpers
  */
 
-;(function($) {
-    // declare var in global scope
+(function($) {
+
     window.JsMoon = {};
+
+    JsMoon.params = {
+        notifications : {
+            selector : '[data-role="notification"]',
+            globalStyles : {
+                'position' : 'absolute',
+                'left' : '0px',
+                'top' : '0px',
+                'right' : '0px',
+                'background-color' : 'rgb(255,150,0)',
+                'color' : '#FFF'
+            }
+        }
+    }
+
+    JsMoon.notifications = {
+        init: function()
+        {
+
+            var selector = JsMoon.params.notifications.selector;
+            var styles = JsMoon.params.notifications.globalStyles;
+            $(selector).hide().css(styles);
+        },
+        run: function()
+        {
+            $(JsMoon.params.notifications.selector).slideDown('slow',function(){
+                $(this).find('[data-action="close"]').first().click(function(){
+                    JsMoon.notifications.blabla($(this));
+                });
+            });
+
+        },
+        blabla: function(element)
+        {
+            element.slideUp();
+        }
+    }
 
     JsMoon.run = function()
     {
+        
+
     	console.log('JsMoon is now running...');
+        JsMoon.notifications.init();
+        JsMoon.notifications.run();
+
+
     }
 
-    JsMoon.SUBNAME = {
-        myFunction: function()
-        {
-            console.log('running JsMoon.SUBNAME.myFunction...');
-        }
-    }
+    
  
 })(jQuery);
 
